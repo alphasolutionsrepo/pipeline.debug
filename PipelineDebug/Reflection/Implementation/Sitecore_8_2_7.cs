e57ebdda088bd07e29ef2e1f3381c9ce8e9f7bd9
Morten Engel <me@alpha-solutions.dk>
2018-09-03 11:28:45 +0200

    missing file

---
+++ PipelineDebug/Reflection/Implementation/Sitecore_8_2_7.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.Pipelines;
+using System.Reflection;
+
+namespace PipelineDebug.Reflection.Implementation
+{
+    public class Sitecore_8_2_7 : Base
+    {
+        protected override object GetPipelineMethodProcessor(PipelineMethod method)
+        {
+            return typeof(PipelineMethod).GetField("target", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(method);
+        }
+    }
+}
